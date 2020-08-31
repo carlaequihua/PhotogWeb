8cc243f400789d0559ee81bba06dd469142557a1
Carla Equihua Ramirez <carlaequihua@Carlas-MacBook-Pro-3.local>
2020-08-31 10:51:02 -0500

    adding basics

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
